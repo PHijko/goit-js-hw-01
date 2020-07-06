60fc7f2a34619e0bee3789160e9870db1e65c0bf
Pavel Hijko <pavel.hijko@gmail.com>
2020-07-06 17:09:36 +0300

    changes task03

--- js/task-number_03.js
+++ js/task-number_03.js
@@ -3,7 +3,7 @@ let message = prompt('Введите пароль:');
 
 if (message === null) {
   message = 'Отменено пользователем!';
-} else if (message === 'jqueryismyjam') {
+} else if (message === ADMIN_PASSWORD) {
   message = 'Добро пожаловать!';
 } else {
   message = 'Доступ запрещен, неверный пароль!';
